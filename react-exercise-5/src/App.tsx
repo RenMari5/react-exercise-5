fbf96e741f6dd6219df46562dc8b5d56cd0926c7
Ren <rpaczkowski@gmail.com>
2022-07-27 18:24:30 -0400

    recommitting

---
+++ react-exercise-5/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import "./components/ArraysEx";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ react-exercise-5/src/components/ArraysEx.tsx
@@ -0,0 +1,10 @@
+export const colors: string[] = ["red", "yellow", "green"];
+
+function ColorsList(colors: string[]) {
+  return (
+    <li>
+    colors.map((color) => <li>{colors}</li>)
+</li>)
+}
+
+export default ColorsList;
